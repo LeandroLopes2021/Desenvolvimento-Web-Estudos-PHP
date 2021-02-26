8e5368eaf6dfb9811f0205bf976fa8e1a8ee3803
Leandro Lopes Teixeira <79723400+LeandroLopes2021@users.noreply.github.com>
2021-02-26 17:53:40 -0300

    Add files via upload

---
+++ Rocky - Teste Pratico/Site Mobile/Script.js
@@ -0,0 +1,7 @@
+              //FUNÇÃO DE EXIBIÇÃO DO MENU//
+function onClickMenu()
+{
+	document.getElementById("Menu").classList.toggle("Change");
+	document.getElementById("Nav").classList.toggle("Change");
+	document.getElementById("Menu-bg").classList.toggle("Change-bg")
+}
