aa00aadd8c3a5bc6c47137d353448c79035ceb15
Leandro Lopes Teixeira <79723400+LeandroLopes2021@users.noreply.github.com>
2021-05-25 19:06:59 -0300

    Add files via upload

---
+++ app_lista_tarefas/conexao.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Conexao
+    {
+        private $host = 'localhost';
+        private $dbname = 'app_lista_tarefa';
+        private $user = 'root';
+        private $pass = "";
+
+        public function conectar()
+        {
+            try
+            {
+                $conexao = new PDO("mysql:host=$this->host;dbname=$this->dbname","$this->user","$this->pass");
+
+                return $conexao;
+            }
+            catch (PDOException $e)
+            {
+                echo 'Erro na conexão';
+            }
+        }
+    }
+?>
